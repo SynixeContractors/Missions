91564d2d81c106e5790a43e21f2fd7df8a3b5942
matidp4 <matidp4@gmail.com>
2023-02-12 21:07:54 -0300

    Template update

--- contracts/CO30_Matias_TheTrooper.pja307/do_not_edit/script_component.hpp
+++ contracts/CO30_Matias_TheTrooper.pja307/do_not_edit/script_component.hpp
@@ -2,8 +2,8 @@
 #define COMPONENT mission
 
 // Version
-#define MAJOR 3
-#define MINOR 6
+#define MAJOR 2
+#define MINOR 0
 #define PATCHLVL 0
 
 // Mission
@@ -14,15 +14,3 @@
 //#define DEBUG_MODE_FULL
 
 #include "\x\cba\addons\main\script_macros_mission.hpp"
-
-// ACE3
-#define ACE_PREFIX ace
-
-#define ACEGVAR(module,var) TRIPLES(ACE_PREFIX,module,var)
-#define QACEGVAR(module,var) QUOTE(ACEGVAR(module,var))
-
-#define ACEFUNC(var1,var2) TRIPLES(DOUBLES(ACE_PREFIX,var1),fnc,var2)
-#define QACEFUNC(var1,var2) QUOTE(ACEFUNC(var1,var2))
-
-#define PATHTOACEF(var1,var2) PATHTOF_SYS(\z\ace\addons,var1,var2)
-#define QPATHTOACEF(var1,var2) QUOTE(PATHTOACEF(var1,var2))
