f11319390d2b9a138ffd2ee60e61bba7e86b9cd2
Amenbreaker <83235712+Amenbreaker@users.noreply.github.com>
2023-09-07 04:50:22 -0300

    CO30_Felix_OrdinaryDays (#128)
    * CO30_Felix_OrdinaryDays
    
    * marker change
    
    * briefing clarification
    
    * debug slip fixed
    
    * Update contracts/CO30_Felix_OrdinaryDays.zargabad/edit_me/description.ext
    
    Co-authored-by: BrettMayson <brett@mayson.io>
    
    * trigger conditions updated and other changes
    
    * enemy cap
    
    * NO CUP VEH - NEW MODPACK
    
    ---------
    
    Co-authored-by: BrettMayson <brett@mayson.io>

---
+++ contracts/CO30_Felix_OrdinaryDays.zargabad/do_not_edit/script_component.hpp
@@ -0,0 +1,16 @@
+#define PREFIX synixecontractors
+#define COMPONENT mission
+
+// Version
+#define MAJOR 2
+#define MINOR 0
+#define PATCHLVL 0
+
+// Mission
+#define MISSION_TYPES ["Contract", "Sub-Contract", "Training", "Special"]
+
+// Debug
+#define DEBUG_SYNCHRONOUS
+//#define DEBUG_MODE_FULL
+
+#include "\x\cba\addons\main\script_macros_mission.hpp"
