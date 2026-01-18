392a745732b27f4ea98b261d6a8619d57c15d6d3
Brett Mayson <brett@mayson.io>
2026-01-18 06:50:42 -0600

    campaign test

---
+++ company/CampaignTest.Altis/do_not_edit/script_component.hpp
@@ -0,0 +1,11 @@
+#define PREFIX synixecontractors
+#define COMPONENT mission
+
+// Mission
+#define MISSION_TYPES ["Contract", "Sub-Contract", "Training", "Special"]
+
+// Debug
+#define DEBUG_SYNCHRONOUS
+//#define DEBUG_MODE_FULL
+
+#include "\x\cba\addons\main\script_macros_mission.hpp"
